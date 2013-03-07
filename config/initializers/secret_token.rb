8bfcf208d6e345708914fd9b8e41eb5d6fd6425a
Julen Windows <julentv@gmail.com>
2013-03-07 18:21:52 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmdxinterface::Application.config.secret_token = '091af08aec00c9e3679e3c34a2bceea551adae9b5bc3e295b71b0ab23798742be87325d357d202f492fd61af9e927c14a7ada12a469bc9959cfc206e166d1c57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dmdxinterface::Application.config.session_store :cookie_store, key: '_dmdxinterface_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dmdxinterface::Application.config.session_store :active_record_store
